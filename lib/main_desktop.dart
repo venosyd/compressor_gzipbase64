bda7d021226079569fc81bec45164763ab69668b
Sergio Lisan <sels@venosyd.com>
2020-11-15 00:30:00 -0300

    final modifications

--- lib/main_desktop.dart
+++ lib/main_desktop.dart
@@ -4,6 +4,7 @@
 library compressor.imagem;
 
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:archive/archive.dart';
 import 'package:clipboard/clipboard.dart';
@@ -38,88 +39,117 @@ class _CompressorAppState extends State<CompressorApp> {
   ///
   String compressed = '';
 
+  ///
+  Uint8List bytes;
+
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Container(
           color: Colors.blue,
           padding: const EdgeInsets.all(24),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              //
+              Container(
+                margin: const EdgeInsets.all(8),
+                child: const Text(
+                  'CONVERSOR PNG/JPG PARA GZIPPED BASE64',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              //
+              Container(
+                margin: const EdgeInsets.only(bottom: 32),
+                child: const Text(
+                  'CLIQUE NO TEXTO PARA COPIAR',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+              //
               Expanded(
-                child: Column(
+                child: Row(
                   children: [
                     //
-                    Container(
-                      margin: const EdgeInsets.all(16),
-                      child: const Text(
-                        'CONVERSOR PNG/JPG PARA GZIP BASE64',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                        ),
-                      ),
-                    ),
-                    //
-                    Container(
-                      width: 196,
-                      height: 48,
-                      margin: const EdgeInsets.symmetric(vertical: 32),
-                      child: FlatButton(
-                        onPressed: _upload,
-                        color: const Color(0xFF64B5F6),
-                        child: const Text(
-                          'SUBIR A IMAGEM',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    //
-                    if (compressed.isNotEmpty)
-                      Expanded(
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(12),
-                          ),
-                          child: Material(
-                            color: const Color(0xFFFAFAFA),
-                            child: InkWell(
-                              onTap: _copy,
-                              child: Padding(
-                                padding: const EdgeInsets.all(24),
-                                child: Center(
-                                  child: Text(
-                                    compressed,
-                                    style: const TextStyle(
-                                      color: Color(0xFF777777),
-                                    ),
-                                  ),
+                    SizedBox(
+                      width: 256,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          //
+                          if (bytes != null)
+                            Expanded(
+                              child: Center(
+                                child: Image.memory(bytes, width: 256),
+                              ),
+                            ),
+                          //
+                          Container(
+                            width: 196,
+                            height: 48,
+                            margin: const EdgeInsets.only(top: 32),
+                            child: FlatButton(
+                              onPressed: _upload,
+                              color: const Color(0xFF64B5F6),
+                              child: const Text(
+                                'SUBIR A IMAGEM',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
+                    ),
                     //
-                    if (compressed.isNotEmpty)
-                      Container(
-                        margin: const EdgeInsets.all(32),
-                        child: const Text(
-                          'CLIQUE NO TEXTO PARA COPIAR',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                          ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 32),
+                        child: Column(
+                          children: [
+                            //
+                            if (compressed.isNotEmpty)
+                              Expanded(
+                                child: ClipRRect(
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(12),
+                                  ),
+                                  child: Material(
+                                    color: const Color(0xFFFAFAFA),
+                                    child: InkWell(
+                                      onTap: _copy,
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(24),
+                                        child: Center(
+                                          child: Text(
+                                            compressed,
+                                            style: const TextStyle(
+                                              color: Color(0xFF777777),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                          ],
                         ),
                       ),
+                    )
                   ],
                 ),
-              )
+              ),
             ],
           ),
         ),
@@ -132,9 +162,11 @@ class _CompressorAppState extends State<CompressorApp> {
     );
 
     final gzip = GZipEncoder();
-    final data = picker.toUint8List();
 
-    setState(() => compressed = base64.encode(gzip.encode(data)));
+    setState(() {
+      bytes = picker.toUint8List();
+      compressed = base64.encode(gzip.encode(bytes));
+    });
   }
 
   ///
