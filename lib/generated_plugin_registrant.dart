d209468866014347b3aa2b03acd6dfa52b55a3aa
Sergio Lisan <sergiolisan@gmail.com>
2021-03-22 19:04:10 -0300

    new entities framework update

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -4,6 +4,7 @@
 
 // ignore_for_file: lines_longer_than_80_chars
 
+// ignore: implementation_imports
 import 'package:file_picker/src/file_picker_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 
